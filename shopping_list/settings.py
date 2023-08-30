ea184b4acc6e958fd958bc75025edd5ac0e0a874
Maurice Yang <mauriceyangmy28@gmail.com>
2023-08-30 13:54:28 +0700

    hotfix: Adding ALLOWED_HOST

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5=2ttk72(@amgf)3octiabi@muzub4=6p(+^5n-dj&f93ii$td
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
