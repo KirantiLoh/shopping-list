367f7165a983160def1c57b913b620acfd26a502
Maurice Yang <mauriceyangmy28@gmail.com>
2023-09-16 20:09:06 +0700

    Pushing

--- main/views.py
+++ main/views.py
@@ -27,6 +27,7 @@ def create_product(request):
         form = ProductForm()
     return render(request, 'create_product.html', {'form': form})
 
+
 def show_xml(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
