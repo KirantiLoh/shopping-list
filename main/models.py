2b52c473194724d775daca9db629550020b59028
Maurice Yang <mauriceyangmy28@gmail.com>
2023-09-20 14:05:23 +0700

    feat: Finishing tutorial 3

--- main/models.py
+++ main/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
     def __str__(self):
