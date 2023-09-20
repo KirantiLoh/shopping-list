44865e23d12ea16459b5a642fac087699609af3a
Maurice Yang <mauriceyangmy28@gmail.com>
2023-09-20 13:52:53 +0700

    feat: Finishing tutorial 3

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('xml', views.show_xml, name='XML'),
     path('json', views.show_json, name='JSON'),
     path('xml/<int:id>/', views.show_xml_by_id, name='show_xml_by_id'),
-path('json/<int:id>/', views.show_json_by_id, name='show_json_by_id'), 
+    path('json/<int:id>/', views.show_json_by_id, name='show_json_by_id'), 
+    path('register', views.register, name='register'), 
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_user, name='logout')
 ]
